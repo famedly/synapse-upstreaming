1d5a5289a181b50e886d4ecd5c6edb9fa82b3f38
Jason Little <j.little@famedly.com>
2026-02-11 13:10:54 -0600

    Adjust WorkerSchemaTests to pry a connection out of the database pool for it's test in compliant ways

--- tests/storage/test_rollback_worker.py
+++ tests/storage/test_rollback_worker.py
@@ -18,13 +18,20 @@
 # [This file includes modifications made by New Vector Limited]
 #
 #
+from contextlib import contextmanager
+from typing import Generator
 from unittest import mock
 
 from twisted.internet.testing import MemoryReactor
 
 from synapse.app.generic_worker import GenericWorkerServer
 from synapse.server import HomeServer
-from synapse.storage.database import LoggingDatabaseConnection
+from synapse.storage.database import (
+    DatabasePool,
+    LoggingDatabaseConnection,
+    PsycopgDatabasePool,
+    TwistedDatabasePool,
+)
 from synapse.storage.prepare_database import PrepareDatabaseException, prepare_database
 from synapse.storage.schema import SCHEMA_VERSION
 from synapse.types import JsonDict
@@ -63,41 +70,55 @@ class WorkerSchemaTests(HomeserverTestCase):
 
         return conf
 
+    @contextmanager
+    def _get_connection(
+        self, db_pool: DatabasePool
+    ) -> Generator[LoggingDatabaseConnection]:
+        """
+        Grab a database connection. Different pools require different methods. Use the
+        context manager pattern to ensure closing the connection after.
+        """
+        if isinstance(db_pool, PsycopgDatabasePool):
+            with db_pool._db_pool.connection() as conn:
+                yield LoggingDatabaseConnection(
+                    conn=conn,
+                    engine=db_pool.engine,
+                    default_txn_name="tests",
+                    server_name="test_server",
+                )
+        else:
+            assert isinstance(db_pool, TwistedDatabasePool)
+            db_conn = LoggingDatabaseConnection(
+                conn=db_pool._db_pool.connect(),
+                engine=db_pool.engine,
+                default_txn_name="tests",
+                server_name="test_server",
+            )
+            yield db_conn
+            db_conn.close()
+
     def test_rolling_back(self) -> None:
         """Test that workers can start if the DB is a newer schema version"""
-
         db_pool = self.hs.get_datastores().main.db_pool
-        db_conn = LoggingDatabaseConnection(
-            conn=db_pool._db_pool.connect(),
-            engine=db_pool.engine,
-            default_txn_name="tests",
-            server_name="test_server",
-        )
+        with self._get_connection(db_pool) as db_conn:
+            cur = db_conn.cursor()
+            cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION + 1,))
 
-        cur = db_conn.cursor()
-        cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION + 1,))
+            db_conn.commit()
 
-        db_conn.commit()
-
-        prepare_database(db_conn, db_pool.engine, self.hs.config)
+            prepare_database(db_conn, db_pool.engine, self.hs.config)
 
     def test_not_upgraded_old_schema_version(self) -> None:
         """Test that workers don't start if the DB has an older schema version"""
         db_pool = self.hs.get_datastores().main.db_pool
-        db_conn = LoggingDatabaseConnection(
-            conn=db_pool._db_pool.connect(),
-            engine=db_pool.engine,
-            default_txn_name="tests",
-            server_name="test_server",
-        )
+        with self._get_connection(db_pool) as db_conn:
+            cur = db_conn.cursor()
+            cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION - 1,))
 
-        cur = db_conn.cursor()
-        cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION - 1,))
+            db_conn.commit()
 
-        db_conn.commit()
-
-        with self.assertRaises(PrepareDatabaseException):
-            prepare_database(db_conn, db_pool.engine, self.hs.config)
+            with self.assertRaises(PrepareDatabaseException):
+                prepare_database(db_conn, db_pool.engine, self.hs.config)
 
     def test_not_upgraded_current_schema_version_with_outstanding_deltas(self) -> None:
         """
@@ -105,28 +126,22 @@ class WorkerSchemaTests(HomeserverTestCase):
         but there are still outstanding delta migrations to run.
         """
         db_pool = self.hs.get_datastores().main.db_pool
-        db_conn = LoggingDatabaseConnection(
-            conn=db_pool._db_pool.connect(),
-            engine=db_pool.engine,
-            default_txn_name="tests",
-            server_name="test_server",
-        )
+        with self._get_connection(db_pool) as db_conn:
+            # Set the schema version of the database to the current version
+            cur = db_conn.cursor()
+            cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION,))
 
-        # Set the schema version of the database to the current version
-        cur = db_conn.cursor()
-        cur.execute("UPDATE schema_version SET version = ?", (SCHEMA_VERSION,))
+            db_conn.commit()
 
-        db_conn.commit()
-
-        # Path `os.listdir` here to make synapse think that there is a migration
-        # file ready to be run.
-        # Note that we can't patch this function for the whole method, else Synapse
-        # will try to find the file when building the database initially.
-        with mock.patch("os.listdir", mock.Mock(side_effect=fake_listdir)):
-            with self.assertRaises(PrepareDatabaseException):
-                # Synapse should think that there is an outstanding migration file due to
-                # patching 'os.listdir' in the function decorator.
-                #
-                # We expect Synapse to raise an exception to indicate the master process
-                # needs to apply this migration file.
-                prepare_database(db_conn, db_pool.engine, self.hs.config)
+            # Path `os.listdir` here to make synapse think that there is a migration
+            # file ready to be run.
+            # Note that we can't patch this function for the whole method, else Synapse
+            # will try to find the file when building the database initially.
+            with mock.patch("os.listdir", mock.Mock(side_effect=fake_listdir)):
+                with self.assertRaises(PrepareDatabaseException):
+                    # Synapse should think that there is an outstanding migration file due to
+                    # patching 'os.listdir' in the function decorator.
+                    #
+                    # We expect Synapse to raise an exception to indicate the master process
+                    # needs to apply this migration file.
+                    prepare_database(db_conn, db_pool.engine, self.hs.config)
